4e5a07fbe8703b9374695101088bd10db1cedea2
michael.grote <38253905+quotengrote@users.noreply.github.com>
2020-07-01 15:32:24 +0200

    funktionen eingefügt

---
+++ 1._Lehrjahr/2. Halbjahr/funktionen/flaeche_fkt-petri.cpp
@@ -0,0 +1,44 @@
+// C++ Trainer, 10.11.2016, all rights reserved
+#include <iostream.h>
+
+// Funktionsdeklaration Eingabe, eine Eingabe Variable = gr(gr stellt das Wort dar was bei der Eingabe verlangt wird), gibt einen float wert zurück
+float eingabe(string gr);
+// Funktionsdeklaration Berechnung, nimmt zwei Eingabevariablen, (l)aenge und (b)reite, gibt einen float wert zurück
+float berechne(float l,float b);
+// Funktionsdeklaration ausgabe, void zeigt an das diese Funktion keinen Rückgabewert hat,
+// deshalb wird sie auch Prozedur genannt
+void ausgabe(float f);
+int main()
+{
+  float laenge, breite, flaeche;
+  // Aufruf Funkttion eingabe 2x, jeweils mit anderem Wort, Übergabe der Funkstionsausgabe an jeweils eine Variable
+  laenge=eingabe("Laenge");
+  breite=eingabe("Breite");
+  // Aufruf berechne, mit den beiden Parametern l und b
+  flaeche=berechne(laenge,breite);
+  // Aufruf ausgabe, mit dem Parameter flaeche der den Ausgabewert enthält
+  ausgabe(flaeche);
+
+  getchar();
+  return 0;
+}
+  // Funktion eingabe; gr stellt das Wort dar das der Funktions als Parameter übergeben wird.
+  float eingabe(string gr)
+  {
+   float x;
+   cout<<"Bitte "<<gr<<" eingeben: ";
+   cin>>x;
+   return x;
+  }
+  // Funktion berechne
+ float berechne(float l,float b)
+ {
+  float fl;
+  fl = l * b;
+  return fl;
+ }
+  // Funktion ausgabe
+ void ausgabe(float f)
+ {
+ cout<<"Der Flaecheninhalt betraegt "<<f<<endl;
+ }
---
+++ 1._Lehrjahr/2. Halbjahr/funktionen/funktionen2-ausgabe_und_eingabe_als_funktion.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+// funktionsdeklaration; muss sich immer vor der main befinden
+// das erste float gibt den datentyp des rückgabewertes an, dann der name, dann die variablen die der funktion übergeben werden müssen, erst l dann b
+float berechnung(float l, float b); // ist eine funktion ohne rückgabewert, also prozedur genannt
+
+// ausgabe funktion
+void ausgabe(float ausgabe_zahl);
+
+// eingabe funktion
+string eingabe(float eingabewert);
+
+// beginn main
+using namespace std;
+int main()
+{
+float flaeche=0, laenge=0, breite=0;
+cout<<"Laenge: "<< endl;
+cin>>laenge;
+cout<<"Breite: "<< endl;
+cin>>breite;
+
+eingabe()
+flaeche = berechnung(laenge, breite);  //Funktionsaufruf, übergabe von fläche an "f" und breite an "b", übergabe des rückgabewertes an flaeche
+
+ausgabe(flaeche);
+
+
+
+
+	getchar();
+	return 0;
+}
+
+// Funktion; die funktion selber muss sich außerhalb der main befinden
+float berechnung(float l, float b) // l& b sind die internen variablenbezecihnungen für das was IN die Fuktionübergeben wird, von aßen kommt laenge und breite
+{
+	float f; //deklaration von f als float da es die variable noch nicht gibt
+	f=l*b; //berechnung
+	return f; //rückgabewert ist f
+}
+
+void ausgabe(float ausgabe_zahl) //vorne void weil die funktion keinen rückgabewert hat
+{
+cout<<"Berechnete Flaeche: "<<ausgabe_zahl;
+}
+
+float eingabe ()
+{
+	cout<<"Laenge: "<< endl;
+	cin>>laenge;
+	cout<<"Breite: "<< endl;
+	cin>>breite;
+}
